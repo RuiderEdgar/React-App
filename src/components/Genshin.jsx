4c0771c4d6af6d950dd6cd67fce1299606bc25af
edgarrivera <edgar.riveraj@gmail.com>
2022-05-11 21:11:30 -0500

    characters-cards

--- src/components/Genshin.jsx
+++ src/components/Genshin.jsx
@@ -12,9 +12,25 @@ export const Genshin = () => {
   return (
     <div className="Genshin">
         {personajes.map(characters => (
-            <h2>{characters.name}</h2>
+          <div className='cards-container'>
+            <img className='imgCharacter' src={characters.cardImageURL} alt="image character"/>
+            <div className='txt-container'>
+              <h2>{characters.name}</h2>
+              <p><span className='bold'>Title:</span> {characters.title}</p>
+              <p><span className='bold'>Element:</span> {characters.element}</p>
+              <p><span className='bold'>Stars:</span> {characters.rarity}</p>
+              <p><span className='bold'>Gender:</span> {characters.gender}</p>
+              <p><span className='bold'>Weapon:</span> {characters.weaponType}</p>
+              <p><span className='bold'>Birthday:</span> {characters.birthday}</p>
+              <p>
+                <span className='bold'>Description:</span>
+                <br />
+                <span>{characters.description}</span> 
+              </p>
+            </div>
+          </div>
         ))}
-    </div>
+    </div> 
   )
 }
 
